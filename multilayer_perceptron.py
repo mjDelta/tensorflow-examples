88431a7c5871b806e81b23fb7aba8fdf66bc256c
zmj <1363177498@qq.com>
2017-07-20 02:02:34 -0700

    Change the weights initialization(Change stddev from 1 to 0.05),good perfermance(0.97)!!

--- multilayer_perceptron.py
+++ multilayer_perceptron.py
@@ -7,13 +7,13 @@ mnist=input_data.read_data_sets("/tmp/data/",one_hot=True)
 import tensorflow as tf
 
 lr=0.001
-training_epochs=15
+training_epochs=20
 batch_size=100
 display_step=1
-keep_prob=0.5
+
 
 #Network Parameters
-n_hidden_1=256 #1st layer number of features
+n_hidden_1=512 #1st layer number of features
 n_hidden_2=512#2nd layer number of features
 n_hidden_3=256
 n_input=784#MNIST data input(img shape:28*28)
@@ -22,12 +22,14 @@ n_classes=10#MNIST total classes(0-9 digits)
 #tf Graph input
 x=tf.placeholder("float",[None,n_input])
 y=tf.placeholder("float",[None,n_classes])
+keep_prob=tf.placeholder("float")
+stddev=0.05
 #store layers weights & biases
 weights={
-	"h1":tf.Variable(tf.random_normal([n_input,n_hidden_1])),
-	"h2":tf.Variable(tf.random_normal([n_hidden_1,n_hidden_2])),
-  "h3":tf.Variable(tf.random_normal([n_hidden_2,n_hidden_3])),
-	"out":tf.Variable(tf.random_normal([n_hidden_3,n_classes]))
+	"h1":tf.Variable(tf.random_normal([n_input,n_hidden_1],stddev=stddev)),
+	"h2":tf.Variable(tf.random_normal([n_hidden_1,n_hidden_2],stddev=stddev)),
+  "h3":tf.Variable(tf.random_normal([n_hidden_2,n_hidden_3],stddev=stddev)),
+	"out":tf.Variable(tf.random_normal([n_hidden_3,n_classes],stddev=stddev))
 }
 biases={
 	"b1":tf.Variable(tf.random_normal([n_hidden_1])),
@@ -56,17 +58,18 @@ accuracy=tf.reduce_mean(tf.cast(correct_prediction,"float"))
 with tf.Session() as sess:
 	sess.run(init)
 	for epoch in range(training_epochs):
-		avg_cost=0.
+
 		total_batch=(int)(mnist.train.num_examples/batch_size)
 		for i in range(total_batch):
 			batch_x,batch_y=mnist.train.next_batch(batch_size)
-			_,c=sess.run([optimizer,cost],feed_dict={x:batch_x,y:batch_y})
-			avg_cost+=c/total_batch
+			_,c=sess.run([optimizer,cost],feed_dict={x:batch_x,y:batch_y,keep_prob:0.6})
+
 		if epoch%display_step==0:
-			print ("Epoch:","%04d"%(epoch+1),"cost=","{:.9f}".format(avg_cost))
+			c=sess.run(cost,feed_dict={x:batch_x,y:batch_y,keep_prob:1.0})
+			print ("Epoch:","%04d"%(epoch+1),"cost=","{:.9f}".format(c))
 	print ("Optimization Finished!")
 	#Test model
 
-	print ("Accuracy:",accuracy.eval({x:mnist.test.images,y:mnist.test.labels}))
+	print ("Accuracy:",accuracy.eval({x:mnist.test.images,y:mnist.test.labels,keep_prob:1.0}))
 
 
